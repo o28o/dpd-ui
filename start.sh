c75d94b2be00bb703aa0e19582317fbf9102cd49
Ubu kv <ubu@fdg.com>
2025-05-02 16:49:14 +0500

    updates

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #pkill -f dpd-ui
 #ps -ef | grep dpd-ui | grep -v grep 
 
-.venv/bin/uvicorn exporter.webapp.main:app --host 0.0.0.0 --port 8880 --reload --reload-dir exporter/webapp 
-
-
+.venv/bin/uvicorn exporter.webapp.main:app --host 0.0.0.0 --port 8880 
+#--reload --reload-dir exporter/webapp 
 #tail -f nohup.out
