0688995b4071f7217a96f6831c2b6b51437dcfff
o yoga <o28o@mail.ru>
2025-07-21 08:21:56 -0400

    update from server

--- exporter/webapp/static/extra.js
+++ exporter/webapp/static/extra.js
@@ -508,6 +508,13 @@ if (!tabsToggle || !tabContainer) {
     return;
 }
 
+ document.body.addEventListener('click', (e) => {
+    if (e.target.closest('.copyright')) {
+      e.preventDefault();
+      window.open('https://creativecommons.org/licenses/by-nc-sa/4.0/', '_blank');
+    }
+  }, { passive: true }); // Флаг passive для оптимизации скролла
+
 // Функция для обновления видимости табов
 function updateTabVisibility() {
     const tabsHidden = localStorage.getItem("tabsHidden");
